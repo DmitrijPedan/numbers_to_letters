3dc12facf4d707284211fd32ba469916446a7329
DmitrijPedan <dmitrijpedan84@gmail.com>
2019-12-17 00:04:38 +0200

    add user input with validation

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
 const cl = (arg) => {console.log(arg)};
 cl('///// Price in numbers to letters /////');
+
+const hundreds = [];
+const dozens = [];
+const units = [];
+const currency = [];
+let userInput = prompt('Введите число от 1 до 999', '');
+
+const getArrayOfUserInput = (inp) => {
+    inp == +inp && +inp > 0 && +inp < 1000 ? inp = inp.split('') : alert('Ввод не верен')
+    return inp; 
+}
+
+
+
+
